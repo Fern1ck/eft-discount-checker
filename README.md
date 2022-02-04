34a0dc1de2f6bc052b9190d44618b5f6b3ba04c0
Fernando Baldrich <52502935+Fern1ck@users.noreply.github.com>
2022-02-03 21:55:50 -0300

    Fixed bugs and improved the code.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# eft-discount-checker
-A simple script to notify via Discord Webhooks if there are discounts in the game Escape from Tarkov
+# Escape from Tarkov Discount Checker
+
+A simple script to notify via Discord Webhooks if there are discounts in the game Escape from Tarkov.
+
+## Prerequisites
+
+This project requires [NodeJS](http://nodejs.org/) and [NPM](https://npmjs.org/).
+To make sure you have them available on your machine,
+try running the following command.
+
+```sh
+$ npm -v && node -v
+```
+
+A [Discord Webhook](https://discord.com/developers/docs/resources/webhook) is required to use this script, because that's how it's going to notify that there's discounts active. Make sure you create it first. To create it:
+
+1. Go to your Discord server settings.
+2. Go to Integrations
+3. Click to Webhooks
+4. Click on "New Webhook". A webhook will be created with a random name.
+5. Click on it, change the name if you want and select the text channel this script is going to notify on.
+6. Copy the webhook URL, you'll need to paste in the .env file in step (3)
+
+## Installation
+
+### 1) Start with cloning this repo on your local machine:
+
+```sh
+$ git clone https://github.com/Fern1ck/eft-discount-checker
+$ cd eft-discount-checker
+```
+
+### 2) To install and set up the dependencies, run:
+
+```sh
+$ npm i
+```
+
+### 3) Create a `.env` file
+
+Create a `.env` file in the root of your project and insert
+the following key/value pairs in the following format of `KEY=VALUE`:
+
+```sh
+DISCORD_WEBHOOK = your_webhook_url
+MINUTES_IN_BETWEEN_QUERIES = 60
+```
+
+Replace "your_webhook_url with the url you copied from step (1).
+
+## Usage
+
+```sh
+$ npm start
+```
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { default: axios } = require("axios");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 require("dotenv").config();
-const fs = require("fs");
 
 function setTerminalTitle(title) {
   process.stdout.write(
@@ -11,92 +10,82 @@ function setTerminalTitle(title) {
 }
 
 async function main() {
-  setTerminalTitle("Notificador de descuentos en Escape from Tarkov");
-  if (process.env.HORAS_ENTRE_QUERIES == undefined) {
-    console.log("HORAS_ENTRE_QUERIES no definido en el archivo .env");
+  setTerminalTitle("Escape from Tarkov Discount Checker");
+  if (process.env.MINUTES_IN_BETWEEN_QUERIES == undefined) {
+    console.log("MINUTES_IN_BETWEEN_QUERIES not defined in the .env file");
     process.exit(1);
   }
 
   if (process.env.DISCORD_WEBHOOK == undefined) {
-    console.log("DISCORD_WEBHOOK no definido en el archivo .env");
+    console.log("DISCORD_WEBHOOK  not defined in the .env file");
     process.exit(1);
   }
 
   console.clear();
-  console.log(`Verificando cada ${process.env.HORAS_ENTRE_QUERIES} horas.`);
   let first_iteration = true;
 
   while (true) {
+    if (!first_iteration) {
+      console.log(
+        `Waiting ${process.env.MINUTES_IN_BETWEEN_QUERIES} minutes before checking again...`
+      );
+      await new Promise((r) =>
+        setTimeout(r, 1000 * 60 * process.env.MINUTES_IN_BETWEEN_QUERIES)
+      );
+    }
+    first_iteration = false;
+
     try {
-      if (first_iteration) {
-        await new Promise((r) =>
-          setTimeout(r, 1000 * 60 * 60 * process.env.HORAS_ENTRE_QUERIES)
-        );
-        first_iteration = false;
-      }
       const res = await axios.get(
         "https://www.escapefromtarkov.com/preorder-page"
       );
+      const dom = new JSDOM(res.data);
 
-      function getPrice(divId) {
-        const dom = new JSDOM(res.data);
-        const price =
-          dom.window.document.getElementById(divId).childNodes[1].childNodes[9]
-            .childNodes[3].childNodes[1].childNodes[1].childNodes[0]
-            .textContent;
+      let discountText = dom.window.document.querySelector(
+        "#preorder_standard > div > div.foot > div:nth-child(2) > div.discount.inline > div > div.discount_text"
+      )?.textContent;
 
-        return parseFloat(price);
-      }
-
-      const standard = getPrice("preorder_standard");
-      const leftBehind = getPrice("preorder_left_behind");
-      const prepareForEscape = getPrice("preorder_prepare_for_escape");
-      const edgeOfDarkness = getPrice("preorder_edge_of_darkness");
-
-      const newPrices = [
-        standard,
-        leftBehind,
-        prepareForEscape,
-        edgeOfDarkness,
-      ];
-
-      let isDiscountActive = false;
-      let prices_array = [];
-
-      try {
-        prices_array = fs
-          .readFileSync("previousPrices.txt")
-          .toString()
-          .split("\n");
-      } catch (err) {
-        if (err.code === "ENOENT") {
-          console.log(
-            "No se escontro el archivo para comparar los precios, se creara uno nuevo con los precios actuales"
-          );
-          fs.writeFileSync("previousPrices.txt", newPrices.join("\n"));
-          continue;
-        } else {
-          throw err;
-        }
-      }
-
-      prices_array.forEach((prevPrice, index) => {
-        if (prevPrice > newPrices[index]) {
-          isDiscountActive = true;
-        }
-      });
-
-      if (!isDiscountActive) {
+      if (!discountText) {
+        console.log("No discounts at the moment.");
         continue;
       }
 
-      fs.writeFileSync("previousPrices.txt", newPrices.join("\n"));
+      divIds = [
+        "preorder_standard",
+        "preorder_left_behind",
+        "preorder_prepare_for_escape",
+        "preorder_edge_of_darkness",
+      ];
 
-      await axios.post(process.env.DISCORD_WEBHOOK, {
-        content: `@here Hay descuentos en Escape from Tarkov! Precios actuales:\n\nStandard: $${standard}\nLeft Behind: $${leftBehind}\nPrepare for Escape: $${prepareForEscape}\nEdge of Darkness: $${edgeOfDarkness}`,
+      let normalPrices = [];
+      let discountPrices = [];
+
+      //Fill normalPrices and discountPrices Array
+      divIds.forEach((divId) => {
+        let normalPrice = dom.window.document.querySelector(
+          `#${divId} > div > div.foot > div:nth-child(2) > div.old.price.strikeout.inline > span`
+        )?.textContent;
+
+        let discountPrice = dom.window.document.querySelector(
+          `#${divId} > div > div.foot > div:nth-child(3) > div.price.inline > span`
+        )?.textContent;
+
+        if (!normalPrice || !discountPrice) return null;
+
+        normalPrices.push(parseFloat(normalPrice));
+        discountPrices.push(parseFloat(discountPrice));
       });
 
-      console.log("Hay un descuento! Enviado a discord.");
+      if (normalPrices.length != 4 || discountPrices.length != 4) {
+        console.log("No discounts at the moment.");
+        continue;
+      }
+
+      const content = `@here ${discountText} in Escape from Tarkov!\nhttps://www.escapefromtarkov.com/preorder-page\n\nDiscount Prices:\nStandard: ~~$${normalPrices[0]}~~ **$${discountPrices[0]}**\nLeft Behind: ~~$${normalPrices[1]}~~ **$${discountPrices[1]}**\nPrepare for Escape: ~~$${normalPrices[2]}~~ **$${discountPrices[2]}**\nEdge of Darkness: ~~$${normalPrices[3]}~~ **$${discountPrices[3]}**`;
+
+      await axios.post(process.env.DISCORD_WEBHOOK, { content });
+
+      console.log("There's discounts! Notified on Discord.");
     } catch (err) {
       console.error(err);
     }
